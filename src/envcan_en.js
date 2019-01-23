84167a7ddcb1b4065e26c72a8e0cc5821da5c592
Geof Baum <baum.geoff@gmail.com>
2019-01-23 10:40:28 -0500

    Remove proxy test

--- src/envcan_en.js
+++ src/envcan_en.js
@@ -88,7 +88,6 @@
 	    var proxy = 'proxy.php';
 		
 		var hrrrTemperatureTimeLayer = L.timeDimension.layer.wms.timeseries(temperatureLayer, {
-			//proxy: proxy,
 			updateTimeDimension: true,
 			updateTimeDimensionMode: "replace",
 			requestTimeFromCapabilities: true
